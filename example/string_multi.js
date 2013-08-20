9811e945ad127ac318b855e64f5bf09254eff370
Julian Gruber <julian@juliangruber.com>
2013-08-20 18:21:57 -0400

    regexp based parser, string_multi test passes

---
+++ example/string_multi.js
@@ -0,0 +1,11 @@
+var streamstache = require('..');
+var fs = require('fs');
+
+var tmpl = streamstache(fs.readFileSync(__dirname + '/string_multi.html'));
+tmpl.set('foo', 'bar');
+
+setTimeout(function() {
+  tmpl.set('bar', 'baz');
+}, 500);
+
+tmpl.pipe(process.stdout);
--- index.js
+++ index.js
@@ -12,31 +12,49 @@ function streamstache(tpl) {
   this.tpl = tpl;
   this.idx = 0;
   this.map = {};
-  this.inId = false;
-  this.id = '';
+  this.waiting = false;
 }
 
 inherits(streamstache, Readable);
 
 streamstache.prototype._read = function(n) {
-  if (this.idx >= this.tpl.length) return this.push(null);
+  var self = this;
 
-  do {
-    var c = this.tpl[this.idx];
-    if (c == '{') {
-      this.inId = true;
-    } else if (c == '}') {
-      this.push(this.map[this.id]);
-      this.inId = false;
-      this.id = '';
-    } else if (this.inId) {
-      this.id += c;
+  if (this.waiting) return;
+
+  if (this.idx >= this.tpl.length) return this.push(null);
+  var range = this.tpl.slice(this.idx, this.idx + n);
+
+  function match(reg) {
+    var m = reg.exec(range);
+    if (!m) return false;
+    range = range.slice(m[0].length);
+    self.idx += m[0].length;
+    return m[1];
+  }
+
+  while (true) {
+    var txt = match(/^([^{]+)/);
+    if (txt) this.push(txt);
+
+    var id = match(/^{([^}]+)}/);
+    if (!id && !txt) return;
+    if (!id) continue;
+
+    if (typeof this.map[id] != 'undefined') {
+      this.push(this.map[id]);
     } else {
-      this.push(c);
+      this.waiting = true;
+      this.once(id, function(value) {
+        this.waiting = false;
+        this.push(value);
+      });
+      break;
     }
-  } while (this.idx++ < Math.min(this.idx + n, this.tpl.length))
+  }
 };
 
 streamstache.prototype.set = function(key, value) {
   this.map[key] = value;
+  this.emit(key, value);
 };
