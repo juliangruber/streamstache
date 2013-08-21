072f87ab6da820915e313565af78a6ac9144493e
Julian Gruber <julian@juliangruber.com>
2013-08-21 05:01:17 -0400

    fresh builds

--- dist/streamstache.js
+++ dist/streamstache.js
@@ -8,55 +8,74 @@ var EventEmitter = require('events').EventEmitter;
 
 module.exports = streamstache;
 
-function streamstache(tpl) {
-  if (!(this instanceof streamstache)) return new streamstache(tpl);
+function streamstache(tpl, scope) {
+  if (!(this instanceof streamstache)) return new streamstache(tpl, scope);
   Readable.call(this);
 
   if (typeof tpl != 'string') tpl = tpl.toString();
 
-  this.ee = new EventEmitter;
-  this.tokens = tpl.split(/[{}]/);
-  this.idx = -1;
-  this.scope = {};
-  this.waiting = 0;
-  this.reading = false;
+  var self = this;
+  self.ee = new EventEmitter;
+  self.tokens = tpl.split(/[{}]/);
+  self.idx = -1;
+  self.scope = {};
+  self.waiting = 0;
+  self.streaming = false;
+  self.parsing = false;
+
+  if (scope) {
+    Object.keys(scope).forEach(function(key) {
+      self.set(key, scope[key]);
+    });
+  }
+
+  if (Object.defineProperty) {
+    for (var i = 1; i < self.tokens.length; i += 2) (function (key) {
+      if (self[key]) throw new Error('reserved key: ' + key);
+      Object.defineProperty(self, key, {
+        get: function() { return self.get(key) },
+        set: function(value) { self.set(key, value) }
+      });
+    })(self.tokens[i]);
+  }
 }
 
 inherits(streamstache, Readable);
 
-streamstache.prototype._read = function(n) {
+streamstache.prototype.stream = function(s) {
   var self = this;
+  self.streaming = true;
+  s.on('readable', function() {
+    var buf = s.read();
+    if (buf) self.push(buf);
+  });
+  s.on('end', function() {
+    self.streaming = false;
+    self.parse();
+  });
+};
 
-  if (self.stream && !self.reading) {
-    self.reading = true;
-    self.stream.on('readable', function() {
-      var buf = self.stream.read(n);
-      if (buf) self.push(buf);
-    });
-    self.stream.on('end', function() {
-      self.stream = null;
-      self.reading = false;
-      self.read();
-    });
-    return;
-  }
+streamstache.prototype.parse = function() {
+  this.parsing = true;
+  this._parse();
+  this.parsing = false;
+};
 
-  if (self.waiting) return;
-  if (self.idx >= self.tokens.length) return self.push(null);
-
+streamstache.prototype._parse = function() {
+  var self = this;
   while(++self.idx < self.tokens.length) {
     var token = self.tokens[self.idx];
 
-    if (self.idx % 2 == 0) {
+    if (self.idx % 2 === 0) {
       var text = token;
-      if (!self.push(text)) return;
+      if (!self.push(text)) break;
     } else {
       var id = token;
       if (typeof self.scope[id] != 'undefined') {
         if (self.scope[id] instanceof Stream) {
-          return self.stream = self.scope[id];
+          return self.stream(self.scope[id]);
         } else {
-          if (!self.push(self.scope[id])) return;
+          if (!self.push(self.scope[id])) break;
         }
         continue;
       }
@@ -65,9 +84,7 @@ streamstache.prototype._read = function(n) {
       self.ee.once(id, function(value) {
         self.waiting--;
         if (value instanceof Stream) {
-          self.stream = value;
-          self.push('');
-          self.read();
+          self.stream(value);
         } else {
           self.push(value);
         }
@@ -75,13 +92,31 @@ streamstache.prototype._read = function(n) {
       return;
     }
   }
+
+  if (self.idx >= self.tokens.length) self.push(null);
 };
 
-// todo: don't pause if used in same tick
+streamstache.prototype._read = function() {
+  if (this.parsing || this.streaming || this.waiting) return;
+  if (this.idx >= this.tokens.length - 1) return this.push(null);
+
+  this.parse();
+};
+
 streamstache.prototype.set = function(key, value) {
-  if (value instanceof Stream) value.pause();
+  if (value instanceof Stream) {
+    if (typeof value.read != 'function') {
+      var r = Readable();
+      r.wrap(value);
+      value = r;
+    }
+  }
   this.scope[key] = value;
   this.ee.emit(key, value);
+};
+
+streamstache.prototype.get = function(key) {
+  return this.scope[key];
 };
 
 },{"events":3,"readable-stream/lib/_stream_readable":2,"stream":4,"util":6}],2:[function(require,module,exports){
